248525d30f5952a91eb99171edbdf056a05574a1
nichmmmtunes <87391033+nichmmmtunes@users.noreply.github.com>
2025-06-20 19:36:10 -0400

    Updated PieceDetail.svelte

--- src/components/PieceDetail.svelte
+++ src/components/PieceDetail.svelte
@@ -855,14 +855,14 @@
                   <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                   <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                 </svg>
-                Edit Peace Piece
+                Edit Details
               </a>
               
               <a href="/edit/{piece.id}" use:link class="edit-button">
                 <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2" fill="none">
                   <path d="M15 10l4.553-2.276A1 1 0 0 1 21 8.618v6.764a1 1 0 0 1-1.447.894L15 14M5 18h8a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2z"></path>
                 </svg>
-                Edit Media
+                Edit Piece
               </a>
             </div>
           {/if}
