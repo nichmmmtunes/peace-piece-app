4bff9ecd131aaa1992183a0166db6c3b47cc103e
nichmmmtunes <87391033+nichmmmtunes@users.noreply.github.com>
2025-06-29 14:39:56 -0400

    Updated Explore.svelte

--- src/components/Explore.svelte
+++ src/components/Explore.svelte
@@ -666,7 +666,7 @@
     width: 100%;
     height: 100%;
     color: white;
-    background-color: var(--color-primary-400);
+    background-color: var(--color-primary-500);
     display: flex;
     align-items: center;
     justify-content: center;
