e7490f82fafe8097f8e9aa87e368dba7531a9802
nichmmmtunes <87391033+nichmmmtunes@users.noreply.github.com>
2025-06-29 23:48:19 -0400

    Updated ArtistProfileSettings.svelte

--- src/components/ArtistProfileSettings.svelte
+++ src/components/ArtistProfileSettings.svelte
@@ -268,6 +268,7 @@
             required
             disabled={loading || uploading}
             placeholder="Your artist username"
+            pattern="^\S+$"
           />
           <p class="field-description">This is how your artist profile URL will display.</p>
         </div>
