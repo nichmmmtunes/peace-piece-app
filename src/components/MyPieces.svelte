f8c60a8a90ff15147b9fb4229630f01376dcfcba
nichmmmtunes <87391033+nichmmmtunes@users.noreply.github.com>
2025-06-30 11:33:16 -0400

    Updated MyPieces.svelte

--- src/components/MyPieces.svelte
+++ src/components/MyPieces.svelte
@@ -222,7 +222,7 @@
                     <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                     <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                   </svg>
-                  Edit
+                  Details
                 </a>
                 <a href="/manage/{piece.id}" use:link class="action-button manage">
                   <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
