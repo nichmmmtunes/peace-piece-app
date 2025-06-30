d0365ef788d25fc4f23da28bcaee38231fb96895
nichmmmtunes <87391033+nichmmmtunes@users.noreply.github.com>
2025-06-30 16:09:19 -0400

    Updated Explore.svelte

--- src/components/Explore.svelte
+++ src/components/Explore.svelte
@@ -665,7 +665,7 @@
   .avatar-placeholder-initials {
     width: 100%;
     height: 100%;
-    color: var(--color-neutral-400);
+    color: var(--text-muted);
     background: linear-gradient(45deg, #262626, var(--color-neutral-900));
     display: flex;
     align-items: center;
